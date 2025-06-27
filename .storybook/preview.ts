69096a8ab248c2a91985d59a3e484eb3c16f2cdf
Jack Coventry <jackcoventry@gmail.com>
2025-06-27 13:44:00 +0100

    Added TypeScript to project with configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,11 @@
 /** @type { import('@storybook/react-vite').Preview } */
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
