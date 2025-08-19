afd303970c1e74c4a3b61a9de8498a6459310757
Jack Coventry <jackcoventry@gmail.com>
2025-08-19 17:45:42 +0100

    Added body text utility class

---
+++ src/stories/foundations/BodyText.stories.jsx
@@ -0,0 +1,18 @@
+const Template = () => {
+  return (
+    <div className="body-text">
+      <p>This is a paragraph with body text styling.</p>
+      <p>Another paragraph to demonstrate the body text style.</p>
+    </div>
+  );
+};
+
+export default {
+  title: "Foundations/Body Text",
+  component: Template,
+  argTypes: {},
+};
+
+export const Example = {
+  args: {},
+};
