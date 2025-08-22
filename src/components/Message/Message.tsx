dc86c404a3550e9b7473adbf5c0b214e0262ee77
Jack Coventry <jackcoventry@gmail.com>
2025-08-22 19:39:00 +0100

    Added some semantic colours and started styling Message component

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import "./Message.css";
 
 type MessageProps = {
+  pinned?: boolean;
   status: "info" | "warning" | "error" | "success";
   text: string;
   title: string;
 };
 
-function Message({ status = "info", text, title }: Readonly<MessageProps>) {
+function Message({
+  pinned,
+  status = "info",
+  text,
+  title,
+}: Readonly<MessageProps>) {
   return (
-    <div className="hoam-message" data-status={status}>
+    <div className="hoam-message" data-status={status} data-pinned={pinned}>
       {title && <h4 className="hoam-message__title">{title}</h4>}
       {text && <p className="hoam-message__text">{text}</p>}
     </div>
