a84a2d67681273cf953e449ad868669c610a9d83
Jack Coventry <jackcoventry@gmail.com>
2025-11-10 16:44:50 +0000

    Added structural styles, separating mobile from desktop markup

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -17,37 +17,45 @@ function Navigation({ items }: Readonly<NavigationProps>) {
   const panelId = (id: string) => `${idPrefix}-panel-${id}`;
   const buttonId = (id: string) => `${idPrefix}-button-${id}`;
 
+  const [mobileNavigationVisible, setMobileNavigationVisibility] = React.useState(false);
+
   return (
     <header className="hoam-navigation">
-      <div
-        className="container"
-        data-visible-on="mobile"
-      >
-        <div className="grid">
-          <div className="span-12">
-            <div className="hoam-navigation__inner">
-              <a
-                href="/"
-                className="hoam-navigation__logo"
-              >
-                <img
-                  src="/logo.svg"
-                  alt="Hoam Logo"
-                />
-              </a>
-              <button
-                className="hoam-navigation__toggle"
-                aria-label="Toggle menu"
-              >
-                Menu
-              </button>
+      <div className="hoam_navigation__mobile">
+        <div className="container-fluid">
+          <div className="grid">
+            <div className="span-12">
+              <div className="hoam-navigation__inner">
+                <a
+                  href="/"
+                  className="hoam-navigation__logo"
+                >
+                  <img
+                    src="/logo.svg"
+                    alt="Hoam Logo"
+                  />
+                </a>
+                <button
+                  className="hoam-navigation__toggle"
+                  aria-label={mobileNavigationVisible ? 'Close menu' : 'Open menu'}
+                  onClick={() => setMobileNavigationVisibility(!mobileNavigationVisible)}
+                >
+                  <svg
+                    className="icon"
+                    width="1.25em"
+                    height="1.25em"
+                    fill="currentColor"
+                  >
+                    <use xlinkHref={`/icons/icons.svg#three-dots-vertical`} />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div
-        data-visible-on="mobile"
-        data-state="closed"
+        data-state={mobileNavigationVisible ? 'open' : 'closed'}
         className="hoam-navigation__mobile-menu"
       >
         <nav aria-label="Main navigation">
@@ -76,10 +84,6 @@ function Navigation({ items }: Readonly<NavigationProps>) {
                 )}
               </li>
             ))}
-          </ul>
-        </nav>
-        <nav aria-label="User navigation">
-          <ul className="hoam-navigation__list">
             <li className="hoam-navigation__item">
               <button className="hoam-navigation__link">My Account</button>
             </li>
@@ -92,106 +96,105 @@ function Navigation({ items }: Readonly<NavigationProps>) {
           </ul>
         </nav>
       </div>
-      <div
-        className="container"
-        data-visible-on="desktop"
-      >
-        <div className="grid">
-          <div className="span-12">
-            <div className="hoam-navigation__inner">
-              <nav aria-label="Main navigation">
-                <ul className="hoam-navigation__list">
-                  {items.map((item) => (
-                    <li
-                      key={item.id}
-                      className="hoam-navigation__item"
-                    >
-                      <a
-                        href={item.href}
-                        id={buttonId(item.id)}
-                        aria-controls={item.panel ? panelId(item.id) : undefined}
-                        aria-expanded={item.panel ? 'false' : undefined}
-                        className="hoam-navigation__link"
+      <div className="hoam-navigation__desktop">
+        <div className="container">
+          <div className="grid">
+            <div className="span-12">
+              <div className="hoam-navigation__inner">
+                <nav aria-label="Main navigation">
+                  <ul className="hoam-navigation__list">
+                    {items.map((item) => (
+                      <li
+                        key={item.id}
+                        className="hoam-navigation__item"
                       >
-                        {item.label}
-                      </a>
-                      {item.panel && (
-                        <div
-                          id={panelId(item.id)}
-                          aria-labelledby={buttonId(item.id)}
-                          className="hoam-navigation__panel"
+                        <a
+                          href={item.href}
+                          id={buttonId(item.id)}
+                          aria-controls={item.panel ? panelId(item.id) : undefined}
+                          aria-expanded={item.panel ? 'false' : undefined}
+                          className="hoam-navigation__link"
                         >
-                          {item.panel}
-                        </div>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-              <a
-                href="/"
-                className="hoam-navigation__logo"
-              >
-                <img
-                  src="/logo.svg"
-                  alt="Hoam Logo"
-                />
-              </a>
-              <nav aria-label="User navigation">
-                <ul
-                  className="hoam-navigation__list"
-                  data-alignment="right"
+                          {item.label}
+                        </a>
+                        {item.panel && (
+                          <div
+                            id={panelId(item.id)}
+                            aria-labelledby={buttonId(item.id)}
+                            className="hoam-navigation__panel"
+                          >
+                            {item.panel}
+                          </div>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </nav>
+                <a
+                  href="/"
+                  className="hoam-navigation__logo"
                 >
-                  <li className="hoam-navigation__item">
-                    <a
-                      href="/account"
-                      className="hoam-navigation__link"
-                      title="My Account"
-                    >
-                      <svg
-                        className="icon"
-                        width="1.25em"
-                        height="1.25em"
-                        fill="currentColor"
+                  <img
+                    src="/logo.svg"
+                    alt="Hoam Logo"
+                  />
+                </a>
+                <nav aria-label="User navigation">
+                  <ul
+                    className="hoam-navigation__list"
+                    data-alignment="right"
+                  >
+                    <li className="hoam-navigation__item">
+                      <a
+                        href="/account"
+                        className="hoam-navigation__link"
+                        title="My Account"
                       >
-                        <use xlinkHref={`/icons/icons.svg#person-circle`} />
-                      </svg>
-                    </a>
-                  </li>
-                  <li className="hoam-navigation__item">
-                    <a
-                      href="/basket"
-                      className="hoam-navigation__link"
-                      title="My basket"
-                    >
-                      <svg
-                        className="icon"
-                        width="1.25em"
-                        height="1.25em"
-                        fill="currentColor"
+                        <svg
+                          className="icon"
+                          width="1.25em"
+                          height="1.25em"
+                          fill="currentColor"
+                        >
+                          <use xlinkHref={`/icons/icons.svg#person-circle`} />
+                        </svg>
+                      </a>
+                    </li>
+                    <li className="hoam-navigation__item">
+                      <a
+                        href="/basket"
+                        className="hoam-navigation__link"
+                        title="My basket"
                       >
-                        <use xlinkHref={`/icons/icons.svg#bag`} />
-                      </svg>
-                    </a>
-                  </li>
-                  <li className="hoam-navigation__item">
-                    <a
-                      href="/search"
-                      className="hoam-navigation__link"
-                      title="Search"
-                    >
-                      <svg
-                        className="icon"
-                        width="1.25em"
-                        height="1.25em"
-                        fill="currentColor"
+                        <svg
+                          className="icon"
+                          width="1.25em"
+                          height="1.25em"
+                          fill="currentColor"
+                        >
+                          <use xlinkHref={`/icons/icons.svg#bag`} />
+                        </svg>
+                      </a>
+                    </li>
+                    <li className="hoam-navigation__item">
+                      <a
+                        href="/search"
+                        className="hoam-navigation__link"
+                        title="Search"
                       >
-                        <use xlinkHref={`/icons/icons.svg#search`} />
-                      </svg>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+                        <svg
+                          className="icon"
+                          width="1.25em"
+                          height="1.25em"
+                          fill="currentColor"
+                        >
+                          <use xlinkHref={`/icons/icons.svg#search`} />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
